efe5715d94658ed00a4c44974ad87a694d7473c8
Isaac Morneau <isaacmorneau@gmail.com>
2018-08-26 04:24:32 -0700

    working hello world sample

--- compile.py
+++ compile.py
@@ -23,55 +23,77 @@ def parse_package(folder):
 
     return allfiles
 
-if __name__ == "__main__":
+def build_sample(packages):
     sample = {}
-    packages = get_packages()
     for p in packages:
-        print("package: {}".format(p))
         pack = parse_package(p)
-        print("    files: {}".format(pack))
         sample[p] = pack
+    return sample
 
-    reincludes = re.compile(r"{<includes>}")
+def build_main(sample):
+    #TODO generalize the regex to be based on dictionary lookups for any file
     cincludes = ""
+    cinit = ""
+    cbreak = ""
+    cclose = ""
+    cmain = ""
+
+    #the regex replacement of the main template
+    reincludes = re.compile(r"{<includes>}")
 
     reinit = re.compile(r"{<init>}")
-    cinit = ""
 
     rebreak = re.compile(r"{<break>}")
     rebreakaround = re.compile(r"^[ ]*{<\?break>}(.|\n)*{<!break>}$", re.MULTILINE)
     resbreak = re.compile(r"{<\?break>}")
     reebreak = re.compile(r"{<!break>}")
-    cbreak = ""
 
     reclose = re.compile(r"{<close>}")
-    cclose = ""
 
-    mainstr = ""
+
     with open("packages/main.c.template") as main_file:
-        mainstr = main_file.read()
+        cmain = main_file.read()
 
-
-    #loop through all the selected samples and then add them all into their replacement strings
-    #once thats happened generate out the new main file via the template replacement
-
-    #for p in sample:
-    #    if "init.h" in p:
+    for p in sample:
+        csamp = sample[p]
+        #collect and join includes
+        cincludes += "\n".join(["#include \"{}/{}\"".format(p, h) for h in sample[p] if h[-2:] == ".h"])
+        if "init.h" in sample[p]:
+            cinit += "{}_init();\n".format(p)
+        if "break.h" in sample[p]:
+            cbreak += "{}_break();\n".format(p)
+        if "close.h" in sample[p]:
+            cclose += "{}_close();\n".format(p)
 
     #simple replacement of includes, init, and cleanup
-    mainstr = reincludes.sub(cincludes, mainstr)
-    mainstr = reinit.sub(cinit, mainstr)
-    mainstr = reclose.sub(cclose, mainstr)
+    cmain = reincludes.sub(cincludes, cmain)
+    cmain = reinit.sub(cinit, cmain)
+    cmain = reclose.sub(cclose, cmain)
 
     #if there are no breaks, remove the loop
     if len(cbreak) == 0:
-        mainstr = rebreakaround.sub("", mainstr)
+        cmain = rebreakaround.sub("", cmain)
     else:
-        mainstr = resbreak.sub("", mainstr)
-        mainstr = reebreak.sub("", mainstr)
-        mainstr = rebreak.sub(cbreak, mainstr)
+        cmain = resbreak.sub("", cmain)
+        cmain = reebreak.sub("", cmain)
+        cmain = rebreak.sub(cbreak, cmain)
+    return cmain
+
+def print_sample(sample):
+    print("sample content")
+    for p in sample:
+        print("package: {}".format(p))
+        print("    files: {}".format(", ".join(sample[p])))
+
+if __name__ == "__main__":
+    packages = get_packages()
+    sample = build_sample(packages)
+    print_sample(sample)
+
+    cmain = build_main(sample)
 
     #write the generated main file into main.c
-    print(mainstr)
+    print("generated main:\n")
+    print(cmain)
     with open("packages/main.c", "w+") as main_file:
-        main_file.write(mainstr)
+        main_file.write(cmain)
