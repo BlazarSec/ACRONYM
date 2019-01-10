cc0c8344b4bf8556984a9e2020dade419bc795ca
Isaac Morneau <isaacmorneau@gmail.com>
2019-01-10 18:34:25 +0000

    improved cmake generation

--- src/skeleton.py
+++ src/skeleton.py
@@ -5,6 +5,54 @@ import os
 import sys
 import subprocess
 
+def build_cmakelists(src_path, include_dirs, project_name):
+    src_str = "    \n".join("\"src/" + f + "\"" for f in os.listdir(src_path) if os.path.isfile(os.path.join(src_path,f)) and f.endswith(".c"))
+
+    include_str = "src"
+    if len(include_dirs) > 0:
+        include_dirs += " " + " ".join(include_dirs)
+
+    return """cmake_minimum_required (VERSION 3.0)
+project ({0})
+
+set ({0}_VERSION_MAJOR 1)
+set ({0}_VERSION_MINOR 0)
+
+if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE Release)
+endif()
+
+
+set(CMAKE_C_FLAGS "-Wall -Wextra")
+set(CMAKE_C_FLAGS_DEBUG "-g -O0 -fsanitize=address -fno-omit-frame-pointer")
+set(CMAKE_C_FLAGS_RELEASE "-Ofast -s -fno-ident  -march=native -flto -DNDEBUG")
+
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_C_STANDARD 11)
+
+set(CMAKE_MODULE_PATH ${{CMAKE_MODULE_PATH}} "${{CMAKE_CURRENT_LIST_DIR}}")
+
+add_definitions(-D_POSIX_C_SOURCE=200809L)
+add_definitions(-D_DEFAULT_SOURCE)
+
+include_directories({1})
+
+SET(SOURCES
+    {2}
+    )
+
+add_executable({0} ${{SOURCES}})
+
+if (CMAKE_BUILD_TYPE EQUAL Release)
+    set_target_properties({0} PROPERTIES INTERPROCEDURAL_OPTIMIZATION TRUE POSITION_INDEPENDENT_CODE TRUE)
+endif()
+
+set(THREADS_PREFER_PTHREAD_FLAG ON)
+find_package(Threads REQUIRED)
+
+target_link_libraries({0} rt Threads::Threads)
+
+""".format(project_name, include_str, src_str)
 
 def scaffold_skeleton(base_path, project_name):
     path = base_path
@@ -37,53 +85,6 @@ def scaffold_skeleton(base_path, project_name):
         print("creating file {}".format(gitignore_path))
         with open(gitignore_path, "w") as f:
             f.write("bin/")
-
-    if os.path.exists(cmake_path):
-        print("{} already exists".format(cmake_path))
-    else:
-        print("creating file {}".format(cmake_path))
-        with open(cmake_path, "w") as f:
-            f.write("""cmake_minimum_required (VERSION 3.0)
-project ({0})
-
-set ({0}_VERSION_MAJOR 1)
-set ({0}_VERSION_MINOR 0)
-
-if(NOT CMAKE_BUILD_TYPE)
-	set(CMAKE_BUILD_TYPE Release)
-endif()
-
-
-set(CMAKE_C_FLAGS "-Wall -Wextra")
-set(CMAKE_C_FLAGS_DEBUG "-g -O0 -fsanitize=address -fno-omit-frame-pointer")
-set(CMAKE_C_FLAGS_RELEASE "-Ofast -s -fno-ident  -march=native -flto -DNDEBUG")
-
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_C_STANDARD 11)
-
-set(CMAKE_MODULE_PATH ${{CMAKE_MODULE_PATH}} "${{CMAKE_CURRENT_LIST_DIR}}")
-
-add_definitions(-D_POSIX_C_SOURCE=200809L)
-add_definitions(-D_DEFAULT_SOURCE)
-
-include_directories(src deps/wrappers)
-
-SET(SOURCES
-    "src/main.c"
-    )
-
-add_executable({0} ${{SOURCES}})
-
-if (CMAKE_BUILD_TYPE EQUAL Release)
-    set_target_properties({0} PROPERTIES INTERPROCEDURAL_OPTIMIZATION TRUE POSITION_INDEPENDENT_CODE TRUE)
-endif()
-
-set(THREADS_PREFER_PTHREAD_FLAG ON)
-find_package(Threads REQUIRED)
-
-target_link_libraries({0} rt Threads::Threads)
-
-""".format(project_name))
 
     if os.path.exists(bin_path):
         print("{} already exists".format(bin_path))
@@ -121,6 +122,15 @@ int main(void) {
         print("{} already exists".format(git_path))
     else:
         subprocess.call(['git', 'init', path])
+
+    #call the cmake generator last
+    if os.path.exists(cmake_path):
+        print("{} already exists".format(cmake_path))
+    else:
+        print("creating file {}".format(cmake_path))
+        with open(cmake_path, "w") as f:
+            f.write(build_cmakelists(src_path, [], project_name))
+
 
 if __name__ == "__main__":
     #if you want to run this directly make sure to run it from the root of the repo
