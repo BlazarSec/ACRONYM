55374a35c8cf28418831cfea5bc064e1376b3a32
Isaac Morneau <isaacmorneau@gmail.com>
2019-02-08 11:48:31 -0800

    removed old compile code updated test

--- src/test.py
+++ src/test.py
@@ -3,10 +3,10 @@ import skeleton as sk
 dp = sk.Cmake("demo project")
 
 print("\n====>empty project\n")
-print(str(dp))
+print(dp)
 
 tg = dp.add_target("client")
 tg.add_file("src/main.c")
 
 print("\n====>single target empty project\n")
-print(str(dp))
+print(dp)
