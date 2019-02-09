c30ce437e1a12138490d1f2849f8e69217c3ec3f
Isaac Morneau <isaacmorneau@gmail.com>
2019-02-08 17:48:41 -0800

    reintegrated new system with the ui

--- src/basic_ui.py
+++ src/basic_ui.py
@@ -3,7 +3,6 @@
 
 import logging
 from logging import iprint, eprint, dprint, wprint, nprint
-import skeleton
 import sample
 import os
 import re
@@ -12,16 +11,15 @@ base_menu = """1 - create sample
 2 - load sample
 0 - exit
 """
-loaded_menu= """1 - create sample
+loaded_menu = """1 - create sample
 2 - load sample
-3 - build basic skeleton
-4 - add git repo
-5 - save sample
+3 - save sample
+4 - regenerate sample
+5 - regenerate cmake
 0 - exit
 """
 
 sampletype_menu = """1 - default project
-2 - basic RAT
 0 - back
 """
 
@@ -65,9 +63,8 @@ if __name__ == "__main__":
                 if sample_option == 1:
                     loaded_sample = sample.Sample(name, path)
                     iprint("sample created")
-                elif sample_option == 2:
-                    host = input("server ip or host: ")
-                    loaded_sample = sample.RAT(name, path, host)
+                else:
+                    eprint("unknown sample type")
 
             elif option == 2:
                 path = input("enter project path: ")
@@ -77,18 +74,17 @@ if __name__ == "__main__":
                 except FileNotFoundError:
                     eprint("sample not found")
             elif loaded_sample and option == 3:
-                loaded_sample.scaffold()
-                loaded_sample.cmake()
-                iprint("skeleton built")
-            elif loaded_sample and option == 4:
-                url = input("enter github user/repo or fully qualified url: ")
-                if not fullyre.match(url):
-                    url = "https://github.com/" + url
-                loaded_sample.add_git_repo(url)
-            elif loaded_sample and option == 5:
                 #requires the sample to not be null
                 loaded_sample.pickle()
                 iprint("sample saved")
+            elif loaded_sample and option == 4:
+                loaded_sample.gen_scaffold()
+                iprint("skeleton built")
+                loaded_sample.gen_cmake()
+                iprint("cmake generated")
+            elif loaded_sample and option == 5:
+                loaded_sample.gen_cmake()
+                iprint("cmake generated")
             else:
                 wprint("unknown option")
     except EOFError:
--- src/sample.py
+++ src/sample.py
@@ -3,67 +3,45 @@ Provides sample abstraction in memory and saving/loading samples to disk.
 """
 
 import pickle
-import skeleton
 import subprocess
 import os
 from logging import iprint, eprint, dprint, wprint, nprint
+from cmake import Cmake
+from skeleton import scaffold
 
 class Sample():
     def __init__(self, name, path):
-        #self.core
-        #self.modules
-        self.include_dirs = []
         self.name = name
+
+        #normalize path to not duplicate such as ./name/name
         if os.path.basename(path).lower() != name.lower():
             path = os.path.join(path, name)
         self.path = path
-        self.targets = []
-        self.constants = []
+        self.cmake = Cmake(name)
 
     # pickle self to file
     def pickle(self):
         with open(os.path.join(self.path, ".sample.pickle"), "wb") as f:
             pickle.dump(self, f)
 
-    # just register another path to be included in the cmake
-    def add_local_dir(self, path):
-        self.include_dirs.append(path)
-
-    def add_git_repo(self, url):
-        output = subprocess.getoutput("cd {}; git clone --recursive {}".format(os.path.join(self.path, "deps"), url))
-        lines = output.splitlines()
-        repo = lines[0].split("'")[1]
-        repopath = os.path.join(self.path, "deps", repo)
-
-        nprint(output)
-
-        self.add_local_dir(repopath)
-
-
-    # pass in all the required args from the sample to the cmakebuilder for further parsing
-    def cmake(self):
-        cmakestr = skeleton.build_cmakelists(os.path.join(self.path, "src"), self.include_dirs, self.name, self.targets, self.constants)
+    #(re)generate the cmake file
+    def gen_cmake(self):
         cmake_path = os.path.join(self.path, "CMakeLists.txt")
         if os.path.exists(cmake_path):
-            wprint("{} already exists".format(cmake_path))
+            wprint("{} already exists, regenerating".format(cmake_path))
         else:
             iprint("creating file {}".format(cmake_path))
-            with open(cmake_path, "w") as f:
-                f.write(cmakestr)
+        with open(cmake_path, "w") as f:
+            f.write(str(self.cmake))
 
-    def scaffold(self):
-        skeleton.scaffold_skeleton(self.path, self.name, cmake=False)
+    #build the directory structure
+    def gen_scaffold(self):
+        scaffold(self.path, self.name)
 
     # build a string to use to display the currently selected sample
     # TODO indicate if changes need saving or not
     def prompt(self):
         return "[{}]".format(self.name)
-
-class RAT(Sample):
-    def __init__(self, name, path, host):
-        super().__init__(name, path)
-        self.constants.append(host)
-
 
 # load pickled Sample by path
 def unpickle_from(path):
