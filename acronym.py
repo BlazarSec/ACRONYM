87d2e9b78d020ba7bb437fa6a3c60e5bf9f29793
Isaac Morneau <isaacmorneau@gmail.com>
2019-03-06 09:45:17 -0800

    added automatic scanning for cli generation

--- acronym.py
+++ acronym.py
@@ -9,13 +9,18 @@ if __name__ == "__main__":
     parser.add_argument("name", help="the name of the project to generate")
     parser.add_argument("path", help="path to the project")
     parser.add_argument("mode", help="what operation to perform", choices=["gen","status"])
+    parser.add_argument("--scan", help="when rebuilding, scan the specified folder for sources, defaults to 'src/'", nargs='?', const='src/', default='src/')
 
     args = parser.parse_args()
 
     if args.mode == "gen":
         sam = Sample(args.name, args.path)
-        #TODO find a way to deal with folder paths better
-        sam.cmake.targets.append(Target(args.name, files=["src/main.c"]))
+        if args.scan:
+            scan_path = os.path.join(args.path, args.scan)
+            paths = [os.path.join(args.scan, file) for file in os.listdir(scan_path) if os.path.isfile(os.path.join(scan_path, file)) and file.endswith(".c")]
+            sam.cmake.targets.append(Target(args.name, files=paths))
+        else:
+            sam.cmake.targets.append(Target(args.name))
         sam.gen_scaffold()
         sam.gen_cmake()
         sam.pickle()
